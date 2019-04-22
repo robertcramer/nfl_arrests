5b9eb4b7f0bbee05b085843bdb8d6f5ef06cabea
Robert Cramer <robert.cramer@fivebox.com>
2019-04-22 17:09:37 -0400

    get reactstrap and bootstrap working in react app

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -1,10 +1,11 @@
 import React from 'react'
+import { Button } from 'reactstrap';
 
 class App extends React.Component {
   render() {
     return (
       <div>
-        Hello World!
+        <Button>Hello</Button>
       </div>
     )
   }
