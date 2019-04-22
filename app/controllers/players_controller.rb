3823247600fe5db2534c3b29cd9e94ae8459c1cc
Robert Cramer <robert.cramer@fivebox.com>
2019-04-22 17:55:56 -0400

    create new players controller accessed via api routes

---
+++ app/controllers/players_controller.rb
@@ -0,0 +1,8 @@
+class PlayersController < ApplicationController
+  
+  def index
+  end
+
+  def details
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
   root to: 'home#index'
+
+  namespace :api do
+    get 'players', to: 'players#index'
+    get 'players/:player_name', to: 'players#details'
+  end
 
   get '*path', to: 'home#index'
 end
