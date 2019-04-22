c35780e1f06462b2be074cba65e2c87b0e043997
Robert Cramer <robert.cramer@fivebox.com>
2019-04-22 18:16:26 -0400

    implement basic service to retrieve list of nfl players

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -1,6 +1,8 @@
 class PlayersController < ApplicationController
-  
+
   def index
+    players = ArrestApi.new.top_players
+    render json: players
   end
 
   def details
---
+++ app/services/arrest_api.rb
@@ -0,0 +1,16 @@
+class ArrestApi
+  require 'net/http'
+
+  def initialize
+    @base_url = 'http://nflarrest.com/api/v1'
+  end
+
+  def top_players
+    url = URI.parse("#{@base_url}/player")
+    req = Net::HTTP::Get.new(url.to_s)
+    res = Net::HTTP.start(url.host, url.port) {|http|
+      http.request(req)
+    }
+    JSON.parse(res.body)
+  end
+end
