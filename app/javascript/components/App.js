588794a2d0f8f893dc5254975b7388649a764c5e
Robert Cramer <robert.cramer@fivebox.com>
2019-04-22 17:21:56 -0400

    add basic navbar

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -1,10 +1,13 @@
 import React from 'react'
 import { Button } from 'reactstrap';
+import TopNav from './TopNav'
 
 class App extends React.Component {
   render() {
     return (
       <div>
+      <TopNav/>
+
         <Button>Hello</Button>
       </div>
     )
